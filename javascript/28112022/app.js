1b4660f00744319370db7ba722a3f8d77f32cd2f
Fabio Vige <fabiovige@gmail.com>
2022-11-28 07:56:35 -0300

    variaveis, dados e concatenação

---
+++ javascript/28112022/app.js
@@ -0,0 +1,43 @@
+// console
+console.log('executando console');
+
+// javascript possui tipagem dinamica
+
+// variável do tipo primitivo
+
+var name = 'Fabio' // string
+var ano = 46 // number
+var altura = 1.76 // float
+var live = true // boolean
+var foor = null // null
+var time //undefined  
+
+// variáveis do tipo compostas
+
+// array
+var fruits = ["morango", "abacaxi", "banana", null, 46, 1.76, true]
+
+// objeto
+var person = {
+    name: 'Joao',
+    age: 46,
+    profession: 'Web developer'
+}
+person.name = name
+
+// reatribuindo valor
+name = true;
+
+console.log(fruits[1].slice(2,7))
+
+// descrobindo o tipo de uma variavel
+console.log(typeof fruits)
+
+// concatenação texto e variáveis
+
+var product = {
+    name: 'Camisa',
+    price: 18.90
+}
+var price = 15.60
+console.log('O valor do produto ' + product.name + ' é ' + product.price)
--- javascript/app.js
+++
@@ -1,9 +0,0 @@
-// console
-console.log('ok');
-
-// var
-var name = 'Fabio' // string
-var ano = 46 // integer
-var altura = 1.76 //float
-var live = true //boolean
-
